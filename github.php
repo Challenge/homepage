36050ac15590f535b76d5571a5d10c0cc6fb342c
Malte Nissen <malte.nissen@gmail.com>
2013-10-02 00:00:05 +0200

    Syntax error fixed

--- github.php
+++ github.php
@@ -1,7 +1,7 @@
 <?php
 // This is the post-receive hook that git forwards to us.
 
-$file = 'git.log'
+$file = 'git.log';
 $payload = json_decode($_POST);
 
 file_put_content($file, '\n'.$payload, FILE_APPEND);
