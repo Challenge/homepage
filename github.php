8367caea162538307c5c2086781e1df7a060eeae
Malte Nissen <malte.nissen@gmail.com>
2013-10-01 23:54:03 +0200

    Added logging to post-receive hook

--- github.php
+++ github.php
@@ -1,3 +1,10 @@
 <?php
-`git pull`
+// This is the post-receive hook that git forwards to us.
+
+$file = 'git.log'
+$payload = json_decode($_POST);
+
+file_put_content($file, '\n'.$payload, FILE_APPEND);
+
+`git pull`;
 
